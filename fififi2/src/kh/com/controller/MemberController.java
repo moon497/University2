ab7f124150aae60feadcd8d083e7ef78862d7100
jheroman <31201399+jheroman@users.noreply.github.com>
2017-10-23 19:16:42 +0900

    정혜 Commit

--- fififi2/src/kh/com/controller/MemberController.java
+++ fififi2/src/kh/com/controller/MemberController.java
@@ -30,6 +30,8 @@ public class MemberController {
 	public String main(HttpServletRequest req, Model model) {
 		logger.info("진입");
 		System.out.println("깃허브를 통한 협업!");
+		System.out.println("멤버컨트롤러를 맡고 있는 정혜");
+		
 		//init
 		List<MainBbs> noticeList;
 		List<MainBbs> todayList;
